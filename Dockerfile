930de99f811eb27b106ce2570be7d6c7c5f0c522
TomchukIvan <vanchester1283@gmail.com>
2026-09-09 22:49:39 +0300

    Пересобрал конфигурационные файлы для 8 Задания

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,21 @@
-FROM python:3.11-slim
+FROM cr.yandex/mirror/library/python:3.11-slim
+
 WORKDIR /app
-COPY app.py .
+
+RUN useradd -r -U appuser
+
+COPY . .
+
+RUN --mount=type=secret,id=my_secret \
+    if [ -f /run/secrets/my_secret ]; then \
+        echo "Секрет доступен при сборке:"; \
+        cat /run/secrets/my_secret; \
+    else \
+        echo "Секрет не передан"; \
+    fi
+
+RUN chown -R appuser:appuser /app
+
+USER appuser
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 import os
-import sys
 
-def main():
-    # Чтение переменных окружения
-    app_env = os.getenv("APP_ENV", "development")
-    secret_key = os.getenv("SECRET_KEY", "default_key")
-    db_url = os.getenv("DATABASE_URL", "sqlite:///default.db")
+print("--- Приложение запущено ---")
+# Проверяем, от какого пользователя работает процесс
+print(f"Текущий UID процесса: {os.getuid()}")
 
-    print(f"--- ЗАПУСК ПРИЛОЖЕНИЯ ---")
-    print(f"Текущее окружение (APP_ENV): {app_env}")
-    print(f"Секретный ключ (SECRET_KEY): {secret_key}")
-    print(f"База данных (DATABASE_URL): {db_url}")
-
-    # Пример адаптации поведения (выбор режима отладки)
-    if app_env.startswith("production"):
-        print("РЕЖИМ: Продакшн. Отладка отключена, логирование в строгом режиме.")
-    else:
-        print("РЕЖИМ: Разработка. Включен детальный вывод ошибок (Debug).")
-
-if __name__ == "__main__":
-    main()
+# Проверяем доступность секретного ключа (для Шага 5)
+secret_path = "/run/secrets/my_build_secret"
+if os.path.exists(secret_path):
+    with open(secret_path, "r") as f:
+        print(f"Секрет успешно прочитан во время сборки: {f.read().strip()}")
